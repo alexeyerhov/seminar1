cdec80b7331dc545d59470c1e8bf1f09e8446d34
Alexey <>
2023-09-25 10:23:27 +0300

    Decided second program

---
+++ program2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace program2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+            int countOfNumbers = 3;
+            double max = 0;
+
+
+            for (int i=0;i<countOfNumbers;i++){
+                Console.WriteLine($"Введите {i+1}-ое число");
+                if (i==0){
+                    max = Convert.ToDouble(Console.ReadLine());
+                }
+                else {
+                    double checker = Convert.ToDouble(Console.ReadLine());
+                    if (checker>max) {
+                        max = checker;
+                    }
+                }
+            }
+            Console.WriteLine($"Максимальное число = {max}");
+
+
+        }
+    }
+}
