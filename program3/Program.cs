f0b97e062b52e81768b5e721f1ceaecd45d6f41c
Alexey <>
2023-09-25 10:28:15 +0300

    Decided third program

---
+++ program3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace program3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+            Console.WriteLine("Введите число для проверки его на чётность:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number%2==0){
+                Console.WriteLine("Число чётное.");
+            }
+            else {
+                Console.WriteLine("Число нечётное.");
+            }
+        }
+    }
+}
