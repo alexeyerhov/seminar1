de57e9825faef9b82d412141c865178251c5340f
Alexey <>
2023-09-25 10:08:49 +0300

    Decided 1st programm

---
+++ program1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _4programs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+double firstNumber = 0;
+double secondNumber = 0;
+Console.WriteLine("Введите первое число: ");
+firstNumber = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+secondNumber = Convert.ToDouble(Console.ReadLine());
+
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine("Первое число больше второго.");
+}
+else if (secondNumber > firstNumber)
+{
+    Console.WriteLine("Второе число больше первого.");
+}
+else {
+    Console.WriteLine("Числа равны.");
+}
+            
+        }
+    }
+}
