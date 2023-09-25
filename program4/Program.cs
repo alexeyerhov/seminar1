2cd59dabf0c6b57da5c7adbbfb0c450066141e9a
Alexey <>
2023-09-25 10:33:41 +0300

    Decided 4th program

---
+++ program4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace program4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+           
+            int number = 0;
+            Console.WriteLine("Введите число: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i < number; i++) {
+                if (i%2==0){
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
